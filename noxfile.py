dd9fe09bf749db482ecc2c9e9f217bb153e07488
thebigmunch <mail@thebigmunch.me>
2019-10-14 10:59:11 -0400

    Reuse existing venv for all nox sessions

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,14 @@
 import os
-import shutil
 
 import nox
+
+ON_TRAVIS = 'TRAVIS' in os.environ
 
 py36 = '3.6'
 py37 = '3.7'
 py38 = '3.8'
 
-ON_TRAVIS = 'TRAVIS' in os.environ
+nox.options.reuse_existing_virtualenvs = True
 
 
 @nox.session(reuse_venv=True)
